1e92e6bb6a54beb6d1cfbd607d45d53fe104dcc2
prabhnith <prabhjot.nith@gmail.com>
2019-11-04 14:25:41 +0530

    Remove photo working

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Photo from "./Photo";
+
+function PhotoWall(props) {
+  return (
+    <div className="photoGrid">
+      {props.posts.map((post, index) => (
+        <Photo key={index} post={post} onRemovePhoto={props.onRemovePhoto} />
+      ))}
+    </div>
+  );
+}
+
+export default PhotoWall;
